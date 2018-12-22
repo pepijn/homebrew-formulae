bd1d4b4fa8ef2b988371307c8eea40339d501fb1
Pepijn Looije <p@epij.nl>
2018-12-22 09:17:42 -0200

    Ugly custom emacs daemon runner

---
+++ pepe-emacs.rb
@@ -0,0 +1,51 @@
+class PepeEmacs < Formula
+  head "https://github.com/pepijn/homebrew-formulae.git"
+
+  def install
+    File.open('pepe-emacs', 'w') { |file| file.write(<<~EOS
+#!/usr/bin/env bash
+set -euo pipefail
+
+exec emacsclient --no-wait --create-frame --alternate-editor=/dev/null "$@"
+EOS
+                                                    )
+    }
+
+    bin.install 'pepe-emacs'
+  end
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+
+      <key>RunAtLoad</key>
+      <true/>
+
+      <key>ProgramArguments</key>
+      <array>
+        <string>/usr/local/bin/run-with-log</string>
+        <string>/usr/local/bin/emacs</string>
+        <string>--fg-daemon</string>
+      </array>
+      <key>StandardOutPath</key>
+      <string>/Users/#{ENV['USER']}/Library/Logs/#{name}.log</string>
+      <key>StandardErrorPath</key>
+      <string>/Users/#{ENV['USER']}/Library/Logs/#{name}.log</string>
+
+      <key>EnvironmentVariables</key>
+      <dict>
+        <key>PEPE_AUTOMATIC</key>
+        <string>launchctl</string>
+
+        <key>PATH</key>
+        <string>/bin:/usr/bin:/usr/local/bin</string>
+      </dict>
+    </dict>
+    </plist>
+  EOS
+  end
+end
