aea75089e0b97c441dc94c6af6ecbb4249584241
Pepijn Looije <p@epij.nl>
2019-01-24 09:05:49 -0200

    Add bash-history-backup

---
+++ bash-history-backup.rb
@@ -0,0 +1,45 @@
+class BashHistoryBackup < Formula
+  head "https://github.com/pepijn/bash-history-backup.git"
+
+  def install
+    bin.install "bin/bash-history-backup"
+  end
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+
+      <key>RunAtLoad</key>
+      <true/>
+
+      <key>ProgramArguments</key>
+      <array>
+        <string>/usr/local/bin/run-with-log</string>
+        <string>#{opt_bin}/#{name}</string>
+        <string>#{var}/#{name}</string>
+      </array>
+      <key>StandardOutPath</key>
+      <string>/Users/#{ENV['USER']}/Library/Logs/#{name}.log</string>
+      <key>StandardErrorPath</key>
+      <string>/Users/#{ENV['USER']}/Library/Logs/#{name}.log</string>
+
+      <key>StartInterval</key>
+      <integer>60</integer><!-- seconds -->
+
+      <key>EnvironmentVariables</key>
+      <dict>
+        <key>PEPE_AUTOMATIC</key>
+        <string>launchctl</string>
+
+        <key>PATH</key>
+        <string>/bin:/usr/bin:/usr/local/bin</string>
+      </dict>
+    </dict>
+    </plist>
+  EOS
+  end
+end
